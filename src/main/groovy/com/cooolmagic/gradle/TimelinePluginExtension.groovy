bf7b1b2b04ba7cce79813517b3bdb698cb6b3eea
Abhishek Nath <cooolmagic@gmail.com>
2015-10-24 14:55:25 -0700

    Change thresholdTimeMillis to 1 sec

--- src/main/groovy/com/cooolmagic/gradle/TimelinePluginExtension.groovy
+++ src/main/groovy/com/cooolmagic/gradle/TimelinePluginExtension.groovy
@@ -2,7 +2,7 @@ package com.cooolmagic.gradle
 
 class TimelinePluginExtension {
     boolean enabled = true
-    long thresholdTimeMillis = 5 * 1000
+    long thresholdTimeMillis = 1 * 1000
     List alwaysInclude = []
     List alwaysExclude = []
     String reportsDir = null
